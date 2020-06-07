7720208da6b68a03cd943b67dfec015f65e35626
Fabien Sabatié <fabiensabatie@MacBook-Pro-de-Fabien.local>
2020-06-07 22:38:15 +0200

    [fix] env paths

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import * as Dotenv from 'dotenv';
 import { WebServer } from './services/server';
 import Routes from './routes';
 
-Dotenv.config(); // Fetches the .env file
+Dotenv.config({ path: `${__dirname}/../environments/.env` }); // Fetches the .env file
 Dotenv.config({
 	path:
 		process.env.ENV === 'production'
-			? `${__dirname}/../../.env.production`
-			: `${__dirname}/../../.env.development`,
+			? `${__dirname}/../environments/.env.production`
+			: `${__dirname}/../environments/.env.development`,
 });
 
 const Server = new WebServer({
