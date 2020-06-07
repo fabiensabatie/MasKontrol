a0ebd438df3a11fcb921c1dd4260470c80dd7646
Fabien Sabatié <fabiensabatie@MacBook-Pro-de-Fabien.local>
2020-06-07 22:13:11 +0200

    [feature] Server functionnal + routes handled

--- src/routes/sequences.ts
+++ src/routes/sequences.ts
@@ -2,13 +2,13 @@ import { Server, Sequence } from '../types';
 import { Request, NextFunction, Response } from 'express';
 
 const SequenceMethods: Server.RouteMap = {
-	get: (req: Request, res: Response, next: NextFunction): Promise<Array<Sequence>> => {
+	geto: (req: Request, res: Response, next: NextFunction): Promise<Array<Sequence>> => {
 		return Promise.resolve([]);
 	},
 };
 
 const SequenceRoutes: Array<Server.Route> = [
-	{ method: 'GET', url: '/sequences', callback: SequenceMethods.get },
+	{ method: 'GET', url: '/sequences', callback: SequenceMethods.geto },
 ];
 
 export default SequenceRoutes;
